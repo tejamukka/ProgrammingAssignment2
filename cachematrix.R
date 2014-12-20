fc0046ebd9359d0e28776d513991f04df92fc804
teja.mukka <tejamukka@gmail.com>
2014-12-20 20:25:28 +0530

    pushing the changes in cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	n<-NULL
+  set<-function(z){
+  x<<- z
+  n<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) n<<- solve
+getmatrix<-function() n
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+	n<-x$getmatrix()
+    if(!is.null(n)){
+      message("cached data is getting fetched")
+      return(n)
+    }
+    matrix<-x$get()
+    n<-solve(matrix, ...)
+    x$setmatrix(n)
+    n
 }
+
